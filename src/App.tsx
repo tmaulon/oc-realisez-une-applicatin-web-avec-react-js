a786a37fcdd2561b94c0682cd57e1e14f71d2ded
tmaulon <tmaulon@betomorrow.com>
2020-05-29 14:10:27 +0200

    improve game App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { GuessCount } from "./components/guess-count/guess-count";
 import { Card } from "./components/card/card";
@@ -9,9 +9,38 @@ const SIDE = 6;
 const SYMBOLS = "😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿";
 
 const App = () => {
-	const handleCardClick = (card: string) => {
-		console.log(card, "clicked");
+	const [cards, setCards] = useState<string[]>();
+	const [currentPair, setCurrentPair] = useState<number[]>([]);
+	const [guesses, setGuesses] = useState<number>(0);
+	const [matchedCardIndices, setMatchedCardIndices] = useState<number[]>([]);
+
+	const handleNewPairClosedBy = (index: number) => {
+		if (!cards) return;
+		const newPair = [currentPair[0], index];
+		setCurrentPair(newPair);
+		setGuesses(guesses + 1);
+		const matched = cards[newPair[0]] === cards[newPair[1]];
+		console.log(
+			"cards[newPair[0]] === cards[newPair[1]] : ",
+			cards[newPair[0]] === cards[newPair[1]]
+		);
+		// if (matched) setMatchedCardIndices(matchedCardIndices.concat(newPair));
+		if (matched) setMatchedCardIndices([...matchedCardIndices, ...newPair]);
+		console.log("matchedCardIndices : ", matchedCardIndices);
+		setTimeout(() => setCurrentPair([]), 750);
 	};
+
+	const handleCardClick = (index: number) => {
+		if (currentPair.length === 2) return;
+		if (currentPair.length === 0) {
+			setCurrentPair([index]);
+			return;
+		}
+		console.log("handleCardClick currentPair : ", currentPair);
+
+		handleNewPairClosedBy(index);
+	};
+	const won = cards && matchedCardIndices.length === cards.length;
 
 	const generateCards = () => {
 		const cardsGenerated: string[] = [];
@@ -24,23 +53,37 @@ const App = () => {
 		}
 		return shuffle(cardsGenerated);
 	};
-	const cards = generateCards();
-
+	useEffect(() => {
+		const initializeGame = generateCards();
+		setCards(initializeGame);
+	}, []);
 	console.log("generatedCards", cards);
+
+	const getCardFeedback = (index: number) => {
+		const indexMatched = matchedCardIndices.includes(index);
+		if (currentPair.length < 2) {
+			return indexMatched || index === currentPair[0] ? "visible" : "hidden";
+		}
+		if (currentPair.includes(index)) {
+			return indexMatched ? "justMatched" : "justMismatched";
+		}
+		return indexMatched ? "visible" : "hidden";
+	};
 
 	return (
 		<Memory>
-			<GuessCount guesses={0} />
+			<GuessCount guesses={guesses} />
 			{cards &&
 				cards.map((card, index) => (
 					<Card
 						card={card}
-						feedback="visible"
+						feedback={getCardFeedback(index)}
 						key={index}
-						onClick={handleCardClick}
+						index={index}
+						onClick={() => handleCardClick(index)}
 					/>
 				))}
-			<HallOfFame entries={FAKE_HOF} />
+			{won && <HallOfFame entries={FAKE_HOF} />}
 		</Memory>
 	);
 };
