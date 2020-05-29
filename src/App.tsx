e5b11d2d6eb1a0e43d23816bf3d06dd7c1133edb
tmaulon <tmaulon@betomorrow.com>
2020-05-29 10:22:40 +0200

    implement all components in my App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { GuessCount } from "./components/guess-count/guess-count";
+import { Card } from "./components/card/card";
+import shuffle from "lodash.shuffle";
+import { HallOfFame, FAKE_HOF } from "./components/hall-of-fame/hall-of-fame";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const SIDE = 6;
+const SYMBOLS = "😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿";
+
+const App = () => {
+	const handleCardClick = (card: string) => {
+		console.log(card, "clicked");
+	};
+
+	const generateCards = () => {
+		const cardsGenerated: string[] = [];
+		const cardsNumber: number = SIDE * SIDE;
+		const candidates = shuffle(SYMBOLS);
+		while (cardsGenerated.length < cardsNumber) {
+			const card = candidates.pop();
+			if (!card) return;
+			cardsGenerated.push(card, card);
+		}
+		return shuffle(cardsGenerated);
+	};
+	const cards = generateCards();
+
+	console.log("generatedCards", cards);
+
+	return (
+		<Memory>
+			<GuessCount guesses={0} />
+			{cards &&
+				cards.map((card, index) => (
+					<Card
+						card={card}
+						feedback="visible"
+						key={index}
+						onClick={handleCardClick}
+					/>
+				))}
+			<HallOfFame entries={FAKE_HOF} />
+		</Memory>
+	);
+};
 
 export default App;
+
+const Memory = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	width: 300px;
+	margin: auto;
+	user-select: none;
+`;
